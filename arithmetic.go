f342322b60f16c3d863519c49d789809ce7a7018
Denis Murphy <dmurphy@dairymaster.com>
2021-06-06 16:54:17 +0100

    learning go

---
+++ arithmetic.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	add := 1 + 2
+	println(add)
+	subtract := 10 - 5
+	println(subtract)
+	mod := 5 % 2
+	println(mod)
+	inc := 1
+	inc++
+	println(inc)
+	inc += 5
+	println(inc)
+	inc *= 2
+	println(inc)
+}
